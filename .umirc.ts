3afd1a284cfba0859a44bfcbdb07ede216abbb93
huangyihai <huangyihai@huayun.com>
2022-07-25 18:24:54 +0800

    chore: 添加插件

--- .umirc.ts
+++ .umirc.ts
@@ -9,10 +9,31 @@ export default {
     { exact: true, path: "/login", component: "login" },
     { exact: true, path: "/register", component: "register" },
     { exact: true, path: "/posts/:postId", component: "posts/post" },
+    { exact: true, path: "/topology", component: "topology" },
   ],
-  // plugins: [require.resolve("@umijs/plugins/dist/tailwindcss")],
+  plugins: [
+    // require.resolve("@umijs/plugins/dist/tailwindcss"),
+    require.resolve("@umijs/plugins/dist/dva"),
+    require.resolve("@umijs/plugins/dist/qiankun"),
+    require.resolve("@umijs/plugins/dist/locale"),
+    require.resolve("@umijs/plugins/dist/request"),
+  ],
   // tailwindcss: {},
-  // dva: {},
-  // request: {},
+  dva: {},
+  request: {},
   mock: false,
+  qiankun: {
+    master: {
+      apps: [
+        {
+          name: "app1",
+          entry: "//localhost:8000",
+        },
+        {
+          name: "app2",
+          entry: "//localhost:7002",
+        },
+      ],
+    },
+  },
 };
